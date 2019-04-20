5070374945336276c862b9b1afc5c0c49bc2582e
Kevmsutton <kevmsutton@gmail.com>
2019-04-20 17:01:22 +0100

    add middleware logger, thunk for fetch requests

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,42 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
 import CardList from "../components/CardList";
-import { robots } from "../Robots.js";
 import SearchBox from "../SearchBox";
 import "../index.css";
 import "../App.css";
 import Scroll from "../components/Scroll.js";
 import ErrorBoundry from "../components/ErrorBoundry.js";
-import { setSearchField } from "../actions.js";
+import { setSearchField, requestRobots } from "../actions.js";
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onSearchChange: event => dispatch(setSearchField(event.target.value))
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   };
 };
 
 class App extends React.Component {
-  state = {};
-
-  componentDidMount() {}
-  //   selectedRobots = () => {
-  //     return robots.filter(robot =>
-  //       robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-  //     );
-  //   };
+  componentDidMount() {
+    this.props.onRequestRobots();
+  }
 
   render() {
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const selectedRobots = robots.filter(robot =>
       robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
-    return (
+    return isPending ? (
+      <h1>Loading...</h1>
+    ) : (
       <div className="pa2 tc">
         <h1 className="f1"> Robofriends</h1>
         <SearchBox onSearchChange={onSearchChange} />
